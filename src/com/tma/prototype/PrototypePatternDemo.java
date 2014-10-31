aef9944b57a1790101a3932b877cf5eb8f79d759
Nghi Tran <knightzoro@gmail.com>
2014-10-31 14:09:09 +0700

    111. thu nghiem commit roi bo

--- src/com/tma/prototype/PrototypePatternDemo.java
+++ src/com/tma/prototype/PrototypePatternDemo.java
@@ -4,10 +4,16 @@ package com.tma.prototype;
  * Created by thuynghi on 10/31/2014.
  */
 public class PrototypePatternDemo {
+
     public static void main(String[] args) {
         ShapeCache.loadCache();
 
         Shape cloneShape = ShapeCache.getShape("1");
         System.out.println("Shape : " +cloneShape.getType());
+
+        cloneShape = ShapeCache.getShape("2");
+        System.out.println("Shape : " +cloneShape.getType());
+
+
     }
 }
